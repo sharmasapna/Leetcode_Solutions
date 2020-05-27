9e0e95364b71e57b608bdd4cf1bfadd531ef8e98
Sapna Sharma <sapnasharma@Sapnas-MacBook-Pro.local>
2020-05-27 01:14:15 -0400

    initial commit

---
+++ 451.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def frequencySort(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        from collections import Counter
+        c = Counter(s)
+        """
+        # You can run the code below to understand how the  single line code works
+        print c.items() # see what is in variable c
+        print sorted(c.items()) # got sorted on first element
+        print sorted(c.items(),key = lambda x:x[1]) # we want to sort on frequency i. second element
+        print sorted(c.items(),key = lambda x:x[1],reverse = True) # we want revrse sorting 
+        for a,b in sorted(c.items(),key = lambda x:x[1],reverse = True):
+            print a,b 
+        for a,b in sorted(c.items(),key = lambda x:x[1],reverse = True):
+            print a*b # we want to repeat the letter as per its frequency
+        #joining the result obtained in a single line
+        print ''.join(a*b for a,b in  sorted(c.items(),key = lambda x: x[1],reverse = True))
+        """
+        return ''.join(a*b for a,b in  sorted(c.items(),key = lambda x: x[1],reverse = True))
+            
+        
