9c8e70fcf45a358e7d568242e1e746d36e36d642
Sapna Sharma <sapnasharma@Sapnas-MacBook-Pro.local>
2020-06-04 00:07:07 -0400

    Easy list set

--- 1436.py
+++ 1436.py
@@ -1,9 +1,10 @@
 #1436
-class Solution(object):
+cclass Solution(object):
     def destCity(self, paths):
         """
         :type paths: List[List[str]]
         :rtype: str
+        """
         """
         list_s = [] # list with source
         list_d = [] # list with destination
@@ -13,3 +14,9 @@ class Solution(object):
         for val in list_d:
             if val not in list_s: # checking if the destination is available in source
                 return val
+        """
+        # we can use zip and set to make it run faster
+        a =list(zip(*paths)) # creates a list of tuples of the two indices as follows
+        # original list : [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+        # after using zip: a is [(u'London', u'New York', u'Lima'), (u'New York', u'Lima', u'Sao Paulo')]
+        return (set(a[1])-set(a[0])).pop() # The difference of the two unique lists are returned
