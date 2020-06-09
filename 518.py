bacbce41a841a530b82f2b1862739036a968ba3b
Sapna Sharma <sapnasharma@Sapnas-MacBook-Pro.local>
2020-06-09 18:33:03 -0400

    Medium Dp with 1-d and 2-d solutions

--- 518.py
+++ 518.py
@@ -10,7 +10,7 @@ class Solution(object):
         r = len(coins)+1
         coins = [0] + coins
         
-       
+        # creating a 2-D matrix for storing the results for future use and calculations
         m = [[0 for x in range(c)] for y in range(r)] 
         m[0][0] =1 # base case
      
@@ -21,3 +21,14 @@ class Solution(object):
                 else:
                     m[i][j] = m[i-1][j] + m[i][(j-coins[i])]
         return  m[r-1][c-1]
+        # Since we are using the elements just above the and in the same row
+        # we can do it in 1-D array as follows
+        dp = [0]*(amount) + 1
+        dp[0] = 1 # base case
+        for i in coins:
+            for j in range(i, amount + 1):
+                  dp[j] += dp[j - i]
+        return dp[amount]
+        
+        
+    
