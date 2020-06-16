db1a73c8beba115c3e454176b774d6269d7ee055
Sapna Sharma <sapnasharma@Sapnas-MacBook-Pro.local>
2020-06-16 19:32:39 -0400

    Easy Binary search tree

---
+++ 700.py
@@ -0,0 +1,20 @@
+#700
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        while root  and root.val != val:
+            if val < root.val:
+                root = root.left 
+            else:
+                root = root.right
+        return root
